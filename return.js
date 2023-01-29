93c40fc62efaec44f3bcd35affc6c398d993d733
Sourav halder <souravh093@gmail.com>
2023-01-29 22:09:48 +0600

    return in function

---
+++ return.js
@@ -0,0 +1,11 @@
+function add(number1, number2) {
+  console.log(number1, number2);
+  var sum = number1 + number2;
+  // console.log(sum);
+  return 32;
+}
+
+// add(23, 99);
+
+var total = add(92, 80);
+console.log("Total", total);
