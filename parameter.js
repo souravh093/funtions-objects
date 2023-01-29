ebd721c48fef8378123b69068d0bc4123565409f
Sourav halder <souravh093@gmail.com>
2023-01-29 21:10:42 +0600

    function perameter js

---
+++ parameter.js
@@ -0,0 +1,20 @@
+function bringSingara(money) {
+    console.log("mama singara dan");
+    console.log(money);
+}
+
+bringSingara(250);
+
+function add(num1, num2) {
+    console.log("hello google:", num1, num2);
+}
+
+add(23, 82)
+
+function sum(a, b, c, d, e) {
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+    console.log(sum);
+}
+
+sum(2, 9, 7, 5, 2);
