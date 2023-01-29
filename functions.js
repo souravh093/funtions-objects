d664d183cadfd1bbb162da666272296064a56eaa
Sourav halder <souravh093@gmail.com>
2023-01-29 20:37:39 +0600

    how to declare function

---
+++ functions.js
@@ -0,0 +1,11 @@
+
+// functions declarations
+function startFan() {
+    console.log('stand up');
+    console.log('walk toward the switch');
+    console.log('press the switch');
+}
+
+// call the functions
+startFan();
+startFan();
